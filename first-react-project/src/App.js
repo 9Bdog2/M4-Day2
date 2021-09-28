c268df9b561a78dad084bfac13fb5cd076f96a58
Bogdan Birau <birau.bogdan92@gmail.com>
2021-09-28 16:52:09 +0100

    Updated

---
+++ first-react-project/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MyNavbar from './components/MyNavbar'
+
+function App() {
+  return (
+    <div>
+      <MyNavbar brand="StriveBooks"/>
+    </div>
+  );
+}
+
+export default App;
